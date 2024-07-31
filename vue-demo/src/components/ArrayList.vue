c049aa43d100139b808ea38ea90253d088c47677
zhangshuai2023 <shuaimailbox@163.com>
2024-08-01 07:36:40 +0800

    demoed some feature of Vue

---
+++ vue-demo/src/components/ArrayList.vue
@@ -0,0 +1,27 @@
+<template>
+    <h3>数组变化侦听</h3>
+    <button @click="addListHandler">添加数据</button>
+    <ul>
+        <li v-for="(name,index) in names" :key="index">{{name}}</li>
+    </ul>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                names:['张三','李四','王五'],
+            }
+        },
+        methods:{
+            addListHandler(){
+                // UI 自动更新的方法
+                this.names.push('赵六');
+                // UI 不会自动更新的方法
+                this.names.concat('赵六');
+                // 要更新数组，进行浅拷贝
+                this.names=this.names.concat('赵六');
+            }
+        }
+    }
+</script>
